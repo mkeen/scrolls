2a93bb84e1ebd6ebf65bd118973ddbd9c2144175
Mike Keen <mwk@mikekeen.com>
2023-03-29 18:42:33 -0400

    write fix

--- src/reducers/asset_metadata.rs
+++ src/reducers/asset_metadata.rs
@@ -159,9 +159,10 @@ impl Reducer {
                                             ));
 
                                             if let Ok(json_string) = serde_json::to_string_pretty(&metadata) {
-                                                let metadata_crdt = model::CRDTCommand::AnyWriteWins(
+                                                let metadata_crdt = model::CRDTCommand::LastWriteWins(
                                                     format!("{}.{}", self.config.key_prefix.as_deref().unwrap_or_default(), fingerprint_str),
                                                     model::Value::String(json_string.to_owned()),
+                                                    self.time.slot_to_wallclock(block.slot().to_owned())
                                                 );
 
                                                 let asset_index_crdt = model::CRDTCommand::SetAdd(
