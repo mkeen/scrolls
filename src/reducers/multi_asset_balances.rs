670a5e4f02fdc83f2d4dda4968cf410b1f711150
Mike Keen <mwk@mikekeen.com>
2023-05-12 16:18:09 -0400

    experimental

--- src/reducers/multi_asset_balances.rs
+++ src/reducers/multi_asset_balances.rs
@@ -1,5 +1,5 @@
 use std::panic;
-use pallas::ledger::traverse::{Asset, MultiEraOutput};
+use pallas::ledger::traverse::{Asset, MultiEraOutput, MultiEraTx};
 use pallas::ledger::traverse::{MultiEraBlock};
 use serde::{Deserialize, Serialize};
 
@@ -92,60 +92,69 @@ impl Reducer {
 
     }
 
-    fn process_produced_txo(
+    fn process_block<'b>(
         &self,
         output: &mut super::OutputPort,
-        timestamp: &u64,
-        assets: &Vec<Asset>,
-        stake_or_address: &str,
+        block: &'b MultiEraBlock<'b>
     ) -> Result<(), gasket::error::Error> {
-        let mut fingerprint_tallies: HashMap<String, i64> = HashMap::new();
-        let mut policy_id_buckets: HashMap<String, String> = HashMap::new();
+        let mut fingerprint_tallies: HashMap<String, HashMap<String, i64>> = HashMap::new();
 
-        for asset in assets {
-            if let Asset::NativeAsset(policy_id, asset_name, quantity) = asset {
-                let asset_name = hex::encode(asset_name);
-                if let Ok(fingerprint) = asset_fingerprint([policy_id.clone().to_string().as_str(), asset_name.as_str()]) {
-                    if !fingerprint.is_empty() {
-                        *fingerprint_tallies.entry(fingerprint.clone()).or_insert(0_i64) += (*quantity as i64);
-                        policy_id_buckets.entry(fingerprint.to_string()).or_insert(policy_id.to_string());
-                    }
+        for (_, tx) in block.txs().into_iter().enumerate() {
+            let timestamp = self.time.slot_to_wallclock(block.slot().to_owned());
+            for (_, meo) in tx.produces() {
+                let address = self.stake_or_address_from_address(&meo.address().unwrap());
+                for asset in meo.assets() {
+                    if let Asset::NativeAsset(policy_id, asset_name, quantity) = asset {
+                        let asset_name = hex::encode(asset_name);
+                        if let Ok(fingerprint) = asset_fingerprint([policy_id.clone().to_string().as_str(), asset_name.as_str()]) {
+                            if !fingerprint.is_empty() {
+                                *fingerprint_tallies.entry(address.to_string()).or_insert(HashMap::new()).entry(fingerprint.clone()).or_insert(0_i64) += quantity as i64;
+                            }
+
+                        }
+
+                    };
 
                 }
 
-            };
+            }
 
         }
+
+
 
         let conf_enable_quantity_index = self.config.native_asset_quantity_index.unwrap_or(true);
         let conf_enable_ownership_index = self.config.native_asset_ownership_index.unwrap_or(true);
 
         if !fingerprint_tallies.is_empty() && (conf_enable_quantity_index || conf_enable_ownership_index) {
             let prefix = self.config.key_prefix.clone().unwrap_or("soa-wallet".to_string());
-            let prefix = self.config.key_prefix.clone().unwrap_or("soa-wallet".to_string());
 
             if conf_enable_quantity_index {
-                for (fingerprint, quantity) in fingerprint_tallies.clone() {
-                    let total_asset_count = model::CRDTCommand::HashCounter(
-                        format!("{}.{}.tally", self.config.key_prefix.clone().unwrap_or("soa-wallet".to_string()), stake_or_address),
-                        fingerprint,
-                        quantity
-                    );
+                for (soa, quantity_map) in fingerprint_tallies.clone() {
+                    for (fingerprint, quantity) in quantity_map {
+                        let total_asset_count = model::CRDTCommand::HashCounter(
+                            format!("{}.{}.tally", self.config.key_prefix.clone().unwrap_or("soa-wallet".to_string()), soa),
+                            fingerprint,
+                            quantity
+                        );
 
-                    output.send(total_asset_count.into())?;
+                        output.send(total_asset_count.into())?;
+                    }
 
                 }
 
             }
 
             if conf_enable_ownership_index {
-                for (fingerprint, _) in fingerprint_tallies {
-                    let total_asset_count = model::CRDTCommand::SetAdd(
-                        format!("{}.{}.owns", prefix, stake_or_address),
-                        fingerprint,
-                    );
+                for (soa, quantity_map) in fingerprint_tallies.clone() {
+                    for (fingerprint, _) in quantity_map {
+                        let total_asset_count = model::CRDTCommand::SetAdd(
+                            format!("{}.{}.owns", prefix, soa),
+                            fingerprint,
+                        );
 
-                    output.send(total_asset_count.into())?;
+                        output.send(total_asset_count.into())?;
+                    }
 
                 }
 
@@ -220,34 +229,10 @@ impl Reducer {
         output: &mut super::OutputPort,
     ) -> Result<(), gasket::error::Error> {
         log::info!("running thing");
-        for (_, tx) in block.txs().into_iter().enumerate() {
-            let timestamp = self.time.slot_to_wallclock(block.slot().to_owned());
-            for (_, meo) in tx.produces() {
-                let address = meo.address().unwrap();
-                log::debug!("making call");
-                self.process_produced_txo(
-                    output,
-                    &timestamp,
-                    &meo.assets(),
-                    &self.stake_or_address_from_address(&address),
-                ).unwrap_or_default();
-
-            }
-
-            for (_, mei) in ctx.find_consumed_txos(&tx, &self.policy).unwrap_or_default() {
-                let address = mei.address().unwrap();
-                let stake_or_address = self.stake_or_address_from_address(&address);
-                log::debug!("making call 2");
-                self.process_spent_txo(
-                    output,
-                    &timestamp,
-                    &mei.assets(),
-                    stake_or_address
-                ).unwrap_or_default();
-
-            }
-
-        }
+        self.process_block(
+            output,
+            block,
+        ).unwrap_or_default();
 
         Ok(())
     }
