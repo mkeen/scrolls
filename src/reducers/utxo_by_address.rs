b0780bcfeac774409de50bed963eb2ab3c21d095
Mike Keen <mwk@mikekeen.com>
2023-05-28 21:33:25 -0400

    store the address

--- src/reducers/utxo_by_address.rs
+++ src/reducers/utxo_by_address.rs
@@ -119,6 +119,19 @@ impl Reducer {
 
         if let Ok(raw_address) = &tx_output.address() {
             let soa = self.stake_or_address_from_address(raw_address);
+            if let Ok(bechAddr) = raw_address.to_bech32() {
+                if soa != bechAddr {
+                    let crdt2 = model::CRDTCommand::any_write_wins(
+                        self.config.key_prefix.as_deref(),
+                        format!("s2a.{}", soa),
+                        bechAddr,
+                    );
+
+                    output.send(crdt2.into());
+                }
+
+            }
+
 
             // Basic utxo info
             let crdt = model::CRDTCommand::set_add(
