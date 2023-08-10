df241a971a0e39cf2fcd0af2c45f5fe2dfc80271
Mike Keen <mwk@mikekeen.com>
2023-08-09 23:32:00 -0400

    renamed config

--- src/bin/scrolls/daemon.rs
+++ src/bin/scrolls/daemon.rs
@@ -43,7 +43,7 @@ struct ConfigRoot {
     finalize: Option<crosscut::FinalizeConfig>,
     chain: Option<ChainConfig>,
     policy: Option<crosscut::policies::RuntimePolicy>,
-    blocks: Option<crosscut::blocks::RollbackDataConfig>,
+    blocks: Option<crosscut::blocks::Config>,
 }
 
 impl ConfigRoot {
