7850f7f995cb975eaa7ebb98b2e0a81a264792be
Mike Keen <mwk@mikekeen.com>
2023-08-12 17:54:39 -0400

    ugh

--- src/enrich/skip.rs
+++ src/enrich/skip.rs
@@ -73,6 +73,7 @@ impl gasket::runtime::Worker for Worker {
                 ))?;
             }
             model::RawBlockPayload::RollBack(cbor) => {
+                log::warn!("decided to sckip enrichment {}", cbor.len());
                 self.output
                     .send(model::EnrichedBlockPayload::roll_back(cbor, BlockContext::default()))?;
             }
