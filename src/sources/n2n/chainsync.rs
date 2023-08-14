0a0643ef6ea2c5c2449e8e6d37a81c4632d32ffe
Mike Keen <mwk@mikekeen.com>
2023-08-13 23:55:23 -0400

    fast

--- src/sources/n2n/chainsync.rs
+++ src/sources/n2n/chainsync.rs
@@ -209,9 +209,7 @@ impl gasket::runtime::Worker for Worker {
             true => self.request_next()?,
             false => self.await_next()?,
         };
-
-        return Ok(gasket::runtime::WorkOutcome::Done);
-
+        
         // see if we have points that already reached certain depth
         let ready = self.chain_buffer.pop_with_depth(self.min_depth);
         log::debug!("found {} points with required min depth", ready.len());
